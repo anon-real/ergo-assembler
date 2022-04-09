8fec44a872d334f2acafe7160e6220b2ab43d9ee
anon_real <anon_real_anon@protonmail.com>
2022-04-09 01:01:28 -0700

    Remove stacktrace

--- app/utils/Utils.scala
+++ app/utils/Utils.scala
@@ -4,9 +4,10 @@ import java.io.{PrintWriter, StringWriter}
 
 object Utils {
   def getStackTraceStr(e: Throwable): String = {
-    val sw = new StringWriter
-    val pw = new PrintWriter(sw)
-    e.printStackTrace(pw)
-    sw.toString
+//    val sw = new StringWriter
+//    val pw = new PrintWriter(sw)
+//    e.printStackTrace(pw)
+//    sw.toString
+    e.getMessage
   }
 }
